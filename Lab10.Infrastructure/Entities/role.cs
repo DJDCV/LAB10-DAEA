3693f7e096ed25029bf8f6419bba11f03219993c
DJDCV <deivid.delcarpio@tecsup.edu.pe>
2025-05-24 15:45:26 -0500

    Entidades hechas

---
+++ Lab10.Infrastructure/Entities/role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10.Infrastructure.Entities;
+
+public partial class role
+{
+    public Guid role_id { get; set; }
+
+    public string role_name { get; set; } = null!;
+
+    public virtual ICollection<user_role> user_roles { get; set; } = new List<user_role>();
+}
